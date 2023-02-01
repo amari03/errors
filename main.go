5d14404cc975844b35853cf132a7afcc411c7b91
Addie M. Vasquez <2020151434@ub.edu.bz>
2023-02-01 13:02:31 -0600

    version 2 of area

--- main.go
+++ main.go
@@ -2,19 +2,34 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 )
 
-func area(length float64, width float64) float64 {
+func area(length float64, width float64) (float64, error) {
+	//check if length is greater than 0
+	if length < 0{
+		err := errors.New("you can't have a negative length")
+		return -1, err
+	}
+	if width < 0{
+		err := errors.New("you can't have a negative width")
+		return -1, err
+	}
 	result := length * width
-	return result
+	return result, nil
 }
 
 func main() {
 	length := 5.5
-	width := -10
+	width := 10
 
 	//call area func
-	result := area(length, float64(width))
+	result,err := area(length, float64(width))
+	
+	if err != nil{
+		log.Fatal(err)
+	}
 	fmt.Println(result)
 }
